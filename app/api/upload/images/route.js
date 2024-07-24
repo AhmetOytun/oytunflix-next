ffdd464981473c465edadf2691747cf395750dbf
AhmetOytun <ahmetoytunkurtuldu@gmail.com>
2024-07-24 11:58:32 +0300

    removed console.logs and changed gitignore

--- app/api/upload/images/route.js
+++ app/api/upload/images/route.js
@@ -2,31 +2,45 @@ import { writeFile } from "fs/promises";
 import fs from "fs";
 import path from "path";
 import { NextResponse } from "next/server";
-
+import sharp from "sharp";
 
 export const POST = async (req, res) => {
-    const formData = await req.formData();
-    const uploadsPath = path.join(process.cwd(), "public", "images");
-    const file = formData.get("smallImage");
-    const originalName = file.name;
-    const time = new Date().getTime();
-    const smallImageName = `${time}-${originalName}`;
-  
-    if (!fs.existsSync(uploadsPath)) {
-      await fs.promises.mkdir(uploadsPath, { recursive: true });
+  const formData = await req.formData();
+  const uploadsPath = path.join(process.cwd(), "public", "images");
+  const file = formData.get("smallImage");
+  const originalName = file.name;
+  const time = new Date().getTime();
+  const smallImageName = `${time}-${originalName}`;
+
+  if (!fs.existsSync(uploadsPath)) {
+    await fs.promises.mkdir(uploadsPath, { recursive: true });
+  }
+
+  if (!file) {
+    return NextResponse.json({ error: "No files received." }, { status: 400 });
+  }
+
+  const buffer = Buffer.from(await file.arrayBuffer());
+
+  try {
+    let compressedImageBuffer;
+    const image = sharp(buffer);
+    const metadata = await image.metadata();
+
+    if (metadata.format === 'jpeg') {
+      compressedImageBuffer = await image.jpeg({ quality: 80 }).toBuffer(); 
+    } else if (metadata.format === 'png') {
+      compressedImageBuffer = await image.png({ compressionLevel: 8 }).toBuffer(); 
+    } else if (metadata.format === 'webp') {
+      compressedImageBuffer = await image.webp({ quality: 80 }).toBuffer(); 
+    } else {
+      compressedImageBuffer = buffer;
     }
-  
-    if (!file) {
-      return NextResponse.json({ error: "No files received." }, { status: 400 });
-    }
-  
-    const buffer = Buffer.from(await file.arrayBuffer());
-    try {
-        await writeFile(`${uploadsPath}/${smallImageName}`, buffer);
-      return NextResponse.json({ smallImage:smallImageName, status: 201 });
-    } catch (error) {
-      console.log("Error occurred ", error);
-      return NextResponse.json({ Message: "Failed", status: 500 });
-    }
-  };
-  
+
+    await writeFile(`${uploadsPath}/${smallImageName}`, compressedImageBuffer);
+    return NextResponse.json({ smallImage: smallImageName, status: 201 });
+  } catch (error) {
+    console.log("Error occurred ", error);
+    return NextResponse.json({ Message: "Failed", status: 500 });
+  }
+};
--- utils/database.js
+++ utils/database.js
@@ -6,14 +6,14 @@ export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
 
     if(isConnected){
-        console.log("MongoDB is already connected");
+        //console.log("MongoDB is already connected");
         return;
     }
 
     try{
         await mongoose.connect(process.env.DB_URI)
         isConnected = true;
-        console.log("MongoDB connected")
+        //console.log("MongoDB connected")
     }catch(error){
         console.log(error)
     }
